92d4cffba84b56cc0da4e3ffd6dc93943aea6612
Keval Shah <145671282+Kevalshah91@users.noreply.github.com>
2024-05-12 17:30:34 +0530

    Update Prims.c

--- Prims.c
+++ Prims.c
@@ -1,55 +1,42 @@
-#include <stdio.h>
-
-void main() {
-  int arr[5][5] = {
-      {0, 1, 2, 0, 0},
-      {1, 0, 0, 3, 0},
-      {2, 0, 0, 6, 5},
-      {0, 3, 6, 0, 4},
-      {0, 0, 5, 4, 0}};
-  int selected[100];
-  int cut[100];
-  int i, j, k;
-  int cost, len_sel, len_cut, trav, flag;
-  int min, mini, mincol;
-
-  selected[0] = 0;
-  len_sel = 1;
-  cut[0] = 0;
-  len_cut = 1;
-  cost = 0;
-  printf("Keval Shah 60009220061\n");
-
-  while (len_cut < 5) {
-    min = 1000;     // to represent a large number
-    for (trav = 0; trav < len_sel; trav++) {
-      int ccol = selected[trav];
-      for (i = 0; i < 5; i++) {
-        flag = 0;
-        for (j = 0; j < len_cut; j++) {
-          if (i == cut[j]) {
-            flag = 1;
-            break;  // Exit the loop once a match is found
-          }
-        }
-        if (flag == 1) {
-          continue;
-        } 
-        else {
-          if (arr[i][ccol] < min && arr[i][ccol] != 0) {
-            min = arr[i][ccol];
-            mini = i;
-            mincol = ccol;
-          }
-        }
-      }
+#include<stdio.h>
+#include<stdbool.h>
+#define V 5
+#define max 10000
+void main(){
+    int i,j;
+    int graph[V][V]={
+        { 0, 2, 0, 6, 0 },
+        { 2, 0, 3, 8, 5 },
+        { 0, 3, 0, 0, 7 },
+        { 6, 8, 0, 0, 9 },
+        { 0, 5, 7, 9, 0 }};
+    int src = 0;
+    int n=0;
+    int x,y,min;
+    bool visited[V];
+    // initialise 
+    for(i=0;i<V;i++){
+        visited[i]=false;
     }
-    printf("Connecting %d to %d with cost %d \n", mincol+1, mini+1, min);
-    selected[len_sel] = mini;
-    len_sel++;
-    cut[len_cut] = mini;
-    len_cut++;
-    cost = cost + min;
-  }
-  printf("Final cost is %d \n", cost);
+    visited[src]=true;
+    while(n<V-1){
+        x=0;
+        y=0;
+        min = max;
+        for(i=0;i<V;i++){
+            if(visited[i]){      // if already visited
+                for(j=0;j<V;j++){
+                    if(!visited[j] && graph[i][j] && graph[i][j] < min){     //If next node not visited and path exits and path less than min
+                        min = graph[i][j];
+                        x=i;
+                        y=j;
+                    }
+                }
+            }
+        }
+        printf("%d -> %d  cost  : %d\n",x,y,graph[x][y]);
+        visited[y] = true;
+        n++;
+    }
+
 }
